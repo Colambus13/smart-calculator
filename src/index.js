95a83018604df9d3a8e838db29494961671f0a75
Pavel Shestak <pavelshestak29@gmail.com>
2018-10-07 22:34:08 +0300

    'eleven_task'

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.initialValue = initialValue;
+        this.toString = function(){
+            return eval(this.initialValue);
+        }
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number){
+        this.initialValue += '+' + number;
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number){
+        this.initialValue += '-' + number;
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number){
+        this.initialValue += '*' + number;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number){
+        this.initialValue += '/' + number;
+        return this;
+    }
+
+    pow(number){
+        this.initialValue += '**' + number;
+        return this;
+    }
 }
 
 module.exports = SmartCalculator;
